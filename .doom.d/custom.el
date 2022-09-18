14b063a54e44fc9adaee9f679bc493ef6b6b968a
Keith Butler <20089137@mail.wit.ie>
2022-09-18 15:08:40 +0100

    initial commit

---
+++ .doom.d/custom.el
@@ -0,0 +1,31 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(android-mode-avd "AVD")
+ '(android-mode-sdk-dir "/opt/android-sdk/")
+ '(org-agenda-files nil)
+ '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
+ '(warning-suppress-types '(((yasnippet zombie)) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block (nil :foreground nil :inherit 'fixed-pitch))
+ '(org-checkbox (nil :inherit 'fixed-pitch))
+ '(org-code (nil :inherit '(shadow fixed-pitch)))
+ '(org-document-title ((t (:height 1.3))))
+ '(org-level-1 ((t (:inherit outline-1 :weight extra-bold :height 1.2))))
+ '(org-level-2 ((t (:inherit outline-2 :weight bold :height 1.15))))
+ '(org-level-3 ((t (:inherit outline-3 :weight bold :height 1.12))))
+ '(org-level-4 ((t (:inherit outline-4 :weight bold :height 1.09))))
+ '(org-level-5 ((t (:inherit outline-5 :weight semi-bold :height 1.06))))
+ '(org-level-6 ((t (:inherit outline-6 :weight semi-bold :height 1.03))))
+ '(org-level-7 ((t (:inherit outline-7 :weight semi-bold))))
+ '(org-level-8 ((t (:inherit outline-8 :weight semi-bold))))
+ '(org-meta-line (nil :inherit '(font-lock-comment-face fixed-pitch)))
+ '(org-special-keyword (nil :inherit '(font-lock-comment-face fixed-pitch)))
+ '(org-table (nil :inherit '(shadow fixed-pitch)))
+ '(org-verbatim (nil :inherit '(shadow fixed-pitch))))
